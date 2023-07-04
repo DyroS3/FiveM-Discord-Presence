7f2bf0171da06d7f1103b022b989c6f10bf7edb1
DingYUU <64310374+DyroS3@users.noreply.github.com>
2023-07-04 20:32:27 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,7 +12,7 @@ Config = {} -- DONT TOUCH THIS PLEASE!
     Issues will solely be resolved on the Github. You can also suggest new styles/features
 --]]
 
-Config.Framework = 'QB' -- ESX, QB, STANDALONE | Consider using a framework with this script, as a lot of the functions are based on a framework. Defaults to QBCore
+Config.Framework = 'ESX' -- ESX, QB, STANDALONE | Consider using a framework with this script, as a lot of the functions are based on a framework. Defaults to QBCore
 
 --[[ Change this to whatever you like, previews are available on Github: 
 healthdisplay // QB, ESX and STANDALONE - Shows the player health as coloured squares, if using QB shows a hospital emoji when knocked down. And when dead shows a skull on either.
@@ -22,25 +22,20 @@ custom // QB, ESX, STANDALONE - Shows custom text, if more than one option is pr
 
 Don't like these options? Create an issue on the github with your idea and I may add it.
     --]] 
-Config.Style = 'CHANGEME'
+Config.Style = 'custom'
 -- If using the location style, translate to your server's language.
-Config.LocationText = 'Player is located on:'
-Config.CustomText = {
-    [0] = 'Be sure to join our discord to be whitelisted!',
-    [1] = 'Over a million possibilities, choose yours!',
-    [2] = 'Make sure to have fun!',
-    [3] = "Don't break rules!"
-}
+Config.LocationText = '\n 我的位置:'
+
 
 --Anything you dont want to show up can be left blank.
 Config.Discord = {
-AppId = "CHANGEME", -- The discord application ID.
-BigAsset = 'CHANGEME', -- Name of a rich presence asset, will show up as the big image
-BigText = 'CHANGEME', -- Text that will show when someone hovers over the big image
-SmallAsset = 'CHANGEME', -- Small image asset name
-SmallText = 'CHANGEME', -- Text when hovering over the small imag
-Button1Text = 'CHANGEME', -- Text that will appear on the first button
-Button1Link = 'CHANGEME', -- Link that will open when pressing the first button
-Button2Text = 'CHANGEME', -- Text that will appear on the second button
-Button2Link = 'CHANGEME' -- Link that will open when pressing the second button.
+    AppId = "840500879714811924", -- The discord application ID.
+    BigAsset = 'https://logos-world.net/wp-content/uploads/2021/03/FiveM-Symbol.png', -- Name of a rich presence asset, will show up as the big image
+    BigText = 'FiveM服务器', -- Text that will show when someone hovers over the big image
+    SmallAsset = 'https://logos-world.net/wp-content/uploads/2021/03/FiveM-Symbol.png', -- Small image asset name
+    SmallText = '欢迎游玩', -- Text when hovering over the small imag
+    Button1Text = '🍇进入测试服务器🍇', -- Text that will appear on the first button
+    Button1Link = 'https://cfx.re/join/ev776m', -- Link that will open when pressing the first button
+    Button2Text = '🍈进入正式服务器🍈', -- Text that will appear on the second button
+    Button2Link = 'https://cfx.re/join/xberbm' -- Link that will open when pressing the second button.
 }
